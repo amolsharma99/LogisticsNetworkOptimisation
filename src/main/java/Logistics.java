fe93c5eaaf4b6782a2a680a7212744d110ca6a65
Amol Sharma <amolsharma99@gmail.com>
2013-07-09 14:29:06 +0530

    First commit

---
+++ src/main/java/Logistics.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: amol.sharma
+ * Date: 09/07/13
+ * Time: 2:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+public class Logistics {
+
+}
---
+++ src/test/java/Logisticstest.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: amol.sharma
+ * Date: 09/07/13
+ * Time: 2:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+
+public class Logisticstest {
+}
